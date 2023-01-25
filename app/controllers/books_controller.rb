78ba61bc906f1a473fac85213cbbdbd87ba6ee7a
Doston Turgunov <doston.7194@gmail.com>
2023-01-25 12:37:44 +0100

    Create QuesryOrchestrator

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@
 class BooksController < ApplicationController
 
   def index
-    books = eager_load(filter(sort(paginate(Book.all)))).map do |book|
+    books = orchestrate_query(Book.all).map do |book|
       BookPresenter.new(book, params).fields.embeds
     end
 
